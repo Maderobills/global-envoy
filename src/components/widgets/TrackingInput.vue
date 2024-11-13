e786851875d1c84dd5f0513d29785ae4412f6ee4
mbillsEnterprise <mbillsenterprise60@gmail.com>
2024-11-13 02:07:06 +0000

    well

--- src/components/widgets/TrackingInput.vue
+++ src/components/widgets/TrackingInput.vue
@@ -2,8 +2,8 @@
   <div :style="backgroundImageStyle" class="bg-cover bg-center h-80 flex flex-col justify-between">
     <div class="h-full text-white text-center p-6 bg-black bg-opacity-20 hover:backdrop-blur-sm space-y-6">
       <h1 class="text-4xl font-bold">Track Your Shipment</h1>
-      <div class="w-full h-12 flex justify-center">
-      <input type="text" class="rounded-s h-12 w-80 p-5 text-slate-900 text-md placeholder:Enter your tracking number">
+      <div class="w-fit h-12 flex justify-self-center bg-white ">
+      <input v-model="message" placeholder="Enter your tracking number" class="rounded-s h-12 w-80 p-5 text-slate-900 text-md">
       <button class="bg-slate-900 px-10 font-semibold hover:font-bold rounded-e">Track</button>
     </div>
     
