e9d7c958bddfb66a3aeb01e8a61b773b308e9e90
Dennis Bortier <dennisbortey60@gmail.com>
2024-11-13 11:14:24 +0000

    quickdone

---
+++ src/components/widgets/QuickService.vue
@@ -0,0 +1,41 @@
+<template>
+    <main class="flex grid-cols-3 gap-10 w-full justify-center items-center h-fit p-6">
+        <CardComp
+                v-for="details in cardDetails"
+                :key="details.id"
+                :icon="details.icon"
+                :title="details.title"
+                :desc="details.desc"
+                @click="handleCardClick(details.id)"
+            />
+    </main>
+</template>
+<script setup>
+import { ref } from 'vue';
+import CardComp from './singles/CardComp.vue';
+
+const cardDetails = ref([
+{ 
+        id: 1, 
+        icon: "fi fi-rr-check-in-calendar", 
+        title: "Add Shipment", 
+        desc: 'Create new shipment record and track your delivery' 
+    },
+    { 
+        id: 1, 
+        icon: "fi fi-rr-calculator-money", 
+        title: "Get a Quote", 
+        desc: 'Generate an estimate for cost applied' 
+    },
+    { 
+        id: 1, 
+        icon: "fi fi-rr-check-in-calendar", 
+        title: "Add Shipment", 
+        desc: 'Create new shipment record and track your delivery' 
+    },
+    
+])
+const handleCardClick = (id) => {
+    console.log(`Card ${id} clicked`);
+};
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,14 +1,17 @@
 <template>
     <TrackingInput />
+    <QuickService/>
 </template>
 
 <script>
 import TrackingInput from '@/components/widgets/TrackingInput.vue';
+import QuickService from '@/components/widgets/QuickService.vue';
 
 export default {
   name: 'HomePage',
   components: {
     TrackingInput,
+    QuickService,
   },
 };
 </script>
