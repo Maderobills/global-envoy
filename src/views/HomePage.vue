6e3aa074b4b66ccdc12ed4e32c8b4ca840061d77
Dennis Bortier <dennisbortey60@gmail.com>
2024-11-13 12:03:46 +0000

    send

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,17 +1,22 @@
 <template>
+  <main class="h-dvh overflow-y-scroll">
     <TrackingInput />
-    <QuickService/>
+    <QuickService />
+    <RolesComp />
+  </main>
 </template>
 
 <script>
 import TrackingInput from '@/components/widgets/TrackingInput.vue';
 import QuickService from '@/components/widgets/QuickService.vue';
+import RolesComp from '@/components/widgets/RolesComp.vue';
 
 export default {
   name: 'HomePage',
   components: {
     TrackingInput,
     QuickService,
+    RolesComp,
   },
 };
 </script>
