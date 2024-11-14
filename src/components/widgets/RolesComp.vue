1d484cc0341a212bbd314e8edf2d876a04b13fb0
Dennis Bortier <dennisbortey60@gmail.com>
2024-11-14 13:14:38 +0000

    new

--- src/components/widgets/RolesComp.vue
+++ src/components/widgets/RolesComp.vue
@@ -1,13 +1,5 @@
 <template>
     <main class="w-full flex justify-center">
-        <div class="w-10/12 h-96 bg-gray-400">
-            <div>
-                <h1>Discover possibilities with simplified FedEx Import Rates</h1>
-                <p>Take advantage of this new opportunity to grow your business. With simplified Import Pricing offered
-                    by FedEx, you can now import with ease and without the worry of exchange rate risk when paying
-                    offshore.</p>
-            </div>
-            <img src="https://img.freepik.com/free-vector/flat-design-import-export-infographic_23-2149166368.jpg?" alt="">
-        </div>
+        
     </main>
 </template>
