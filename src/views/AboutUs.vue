d82ae26087a65fb35731d465782a3f415c5b820b
GlobalEnvoy <envoy.glo@gmail.com>
2024-12-05 22:47:05 +0000

    okay

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -3,19 +3,15 @@ const Image1 =
   "https://img.freepik.com/free-photo/vehicles-laptop-supply-chain-representation_23-2149853161.jpg?";
 </script>
 <template>
-  <main
-    class="h-screen w-4/5 overflow-hidden overflow-y-auto justify-self-center"
-  >
+  <main class="h-screen w-4/5 justify-self-center overflow-hidden overflow-y-auto scrollbar-hide">
     <div class="pt-10">
-      <h1 class="text-emerald-600 text-4xl font-bold">COMPANY OVERVIEW</h1>
+      <h1 class="text-emerald-600 text-4xl font-bold text-center">COMPANY OVERVIEW</h1>
     </div>
 
-    <section
-      class="max-w-7xl mx-auto bg-slate-100 p-10 rounded-lg shadow-md mt-8"
-    >
+    <section class="max-w-7xl mx-auto  rounded-lg mt-8">
       <div class="flex flex-col md:flex-row items-start">
         <div class="md:w-2/3">
-          <p class="text-gray-600">
+          <p class="text-gray-600 text-justify">
             Global Envoy is a leading shipping and tracking company dedicated to
             providing seamless logistics solutions across the globe. With a
             commitment to excellence and innovation, we strive to connect
@@ -28,61 +24,36 @@ const Image1 =
           </p>
         </div>
 
-        <div class="md:w-1/3 mt-6 md:mt-0 md:ml-8">
-          <img :src="Image1" alt="Speedaf Truck" class="rounded-lg shadow-md" />
+        <div class="md:w-2/5 h-80 w-full mt-6 md:mt-0 md:ml-8 flex justify-center">
+          <img :src="Image1" alt="Shipping and Logistics" class=" shadow-2xl rounded-lg w-full h-auto max-w-xs" />
         </div>
       </div>
     </section>
 
-    <section
-      class="m-9 relative bg-gradient-to-r from-slate-500 to-slate-900 text-white py-10 mt-10"
-    >
-      <div
-        class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 text-center"
-      >
+    <section class="m-6 rounded-lg relative bg-gradient-to-r from-slate-500 to-slate-900 text-white py-10 mt-10">
+      <div class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
         <div>
-          <div class="w-20 h-20 mx-auto mb-4">
-            <h2 class="pb-8 text-xl font-bold">Vision</h2>
-            <img
-              src="https://img.freepik.com/free-photo/vehicles-laptop-supply-chain-representation_23-2149853161.jpg?"
-              alt="Vision Icon"
-              class="rounded-full w-56 h"
-            />
-          </div>
-
-          <p class="pt-7 mt-2 text-sm">
+          <h2 class="pb-4 text-xl font-bold">Vision</h2>
+          <img :src="Image1" alt="Vision Icon" class="rounded-full w-36 h-36 mx-auto mb-2" />
+          <p class="pt-2 text-sm">
             Become a leading one-stop integrated logistics service provider in
             emerging markets.
           </p>
         </div>
 
         <div>
-          <div class="w-20 h-20 mx-auto mb-4">
-            <h2 class="pb-8 text-xl font-bold">Mission</h2>
-            <img
-              src="https://img.freepik.com/free-photo/vehicles-laptop-supply-chain-representation_23-2149853161.jpg?"
-              alt="Mission Icon"
-              class="rounded-full"
-            />
-          </div>
-
-          <p class="pt-7 mt-2 text-sm">
+          <h2 class="pb-4 text-xl font-bold">Mission</h2>
+          <img :src="Image1" alt="Mission Icon" class="rounded-full w-36 h-36 mx-auto mb-2" />
+          <p class="pt-2 text-sm">
             Optimize the end-to-end logistics chain between China and emerging
             markets for stable and efficient logistics solutions.
           </p>
         </div>
 
         <div>
-          <div class="w-20 h-20 mx-auto mb-4">
-            <h2 class="pb-8 text-xl font-bold">Values</h2>
-            <img
-              src="https://img.freepik.com/free-photo/vehicles-laptop-supply-chain-representation_23-2149853161.jpg?"
-              alt="Core Values Icon"
-              class="rounded-full"
-            />
-          </div>
-
-          <p class="pt-7 mt-2 text-sm">
+          <h2 class="pb-4 text-xl font-bold">Values</h2>
+          <img :src="Image1" alt="Core Values Icon" class="rounded-full w-36 h-36 mx-auto mb-2" />
+          <p class="pt-2 text-sm">
             Integrate advantageous resources to build a reliable logistics
             network.
           </p>
@@ -92,52 +63,52 @@ const Image1 =
 
     <section class="max-w-7xl mx-auto py-10 mt-10">
       <h2 class="text-center text-2xl font-bold mb-8">Corporate Values</h2>
-      <div class="grid grid-cols-2 md:grid-cols-5 gap-6">
+      <div class="grid grid-cols-2 gap-6 md:grid-cols-5">
         <div class="text-center">
-          <img
-            src="https://img.freepik.com/free-photo/vehicles-laptop-supply-chain-representation_23-2149853161.jpg?"
-            alt="Customer First"
-            class="w-16 h-16 mx-auto mb-2"
-          />
+          <img :src="Image1" alt="Customer First" class="w-16 h-16 mx-auto mb-2" />
           <p class="text-sm font-medium">Customer First</p>
         </div>
 
         <div class="text-center">
-          <img
-            src="https://img.freepik.com/free-photo/vehicles-laptop-supply-chain-representation_23-2149853161.jpg?"
-            alt="Humanity and Pragmatism"
-            class="w-16 h-16 mx-auto mb-2"
-          />
+          <img :src="Image1" alt="Humanity and Pragmatism" class="w-16 h-16 mx-auto mb-2" />
           <p class="text-sm font-medium">Humanity and Pragmatism</p>
         </div>
 
         <div class="text-center">
-          <img
-            src="https://img.freepik.com/free-photo/vehicles-laptop-supply-chain-representation_23-2149853161.jpg?"
-            alt="Respect and Assurance"
-            class="w-16 h-16 mx-auto mb-2"
-          />
+          <img :src="Image1" alt="Respect and Assurance" class="w-16 h-16 mx-auto mb-2" />
           <p class="text-sm font-medium">Respect and Assurance</p>
         </div>
 
         <div class="text-center">
-          <img
-            src="https://img.freepik.com/free-photo/vehicles-laptop-supply-chain-representation_23-2149853161.jpg?"
-            alt="Efficient Execution"
-            class="w-16 h-16 mx-auto mb-2"
-          />
+          <img :src="Image1" alt="Perseverance" class="w-16 h-16 mx-auto mb-2" />
           <p class="text-sm font-medium">Perseverance</p>
         </div>
 
         <div class="text-center">
-          <img
-            src="https://img.freepik.com/free-photo/vehicles-laptop-supply-chain-representation_23-2149853161.jpg?"
-            alt="Efficient Execution"
-            class="w-16 h-16 mx-auto mb-2"
-          />
+          <img :src="Image1" alt="Efficient Execution" class="w-16 h-16 mx-auto mb-2" />
           <p class="text-sm font-medium">Efficient Execution</p>
         </div>
       </div>
     </section>
   </main>
 </template>
+
+<style scoped>
+@media (max-width: 640px) {
+  h1 {
+    font-size: 2rem;
+  }
+  h2 {
+    font-size: 1.5rem;
+  }
+}
+
+@media (min-width: 640px) and (max-width: 768px) {
+  h1 {
+    font-size: 2.5rem;
+  }
+  h2 {
+    font-size: 1.75rem;
+  }
+}
+</style>
